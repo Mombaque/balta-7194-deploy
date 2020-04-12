83f0576ebeeee251daa472da0c69ea22dc0dcaa7
Mombaque <borgesdiogo92@gmail.com>
2020-04-11 22:17:08 -0300

    Alteração para exibir exception em produção

--- Startup.cs
+++ Startup.cs
@@ -74,11 +74,9 @@ namespace Shop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                //serve para não expor a URL em Produção
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())            
+            app.UseDeveloperExceptionPage();
+            
 
             app.UseHttpsRedirection();
 
